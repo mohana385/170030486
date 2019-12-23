414cc852d43145c5cbde029343c24ba74fbe3940
mohana385 <59167492+mohana385@users.noreply.github.com>
2019-12-23 16:42:26 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.util.Scanner;
+public class Box{
+   
+double w,h,d; 
+    
+
+Box(double width,double height,double depth){
+
+
+w=width;    
+h=height;
+      
+d=depth;   
+  
+  }
+      
+double volume(){
+double v;
+          
+ v=w*h*d
+ ;
+return v;
+     }
+     
+public static void main(String []args){
+         
+Box bc=new Box(4,4,4);
+    
+System.out.println(bc.volume());
+     
+
+}
+
+}
